55f2474bda5e935304025d982c9d99b6ef3227fd
Roméo Maignal <romeomaignal@Romeos-MacBook-Air-2.local>
2026-05-20 22:46:41 +0200

    feat(viz1&2): python scripts used for the development of viz1 and viz2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import './About.css'
 
 const team = [
   { name: 'Nicolas Karmolinski', contribution: 'Created data visualization ... and ..., ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-  { name: 'Roméo Maignal', contribution: 'Created data visualization ... and ..., ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+  { name: 'Roméo Maignal', contribution: 'Worked on the first skeleton of the website and then embellished it with additional decorations, animations and responsive design features. In charge, of the two first data visualizations, extracting the corresponding data and implementing the interactive features with D3.js' },
   { name: 'Jakub Kielar', contribution: 'Created data visualization ... and ..., ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
 ]
 
